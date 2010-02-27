a0901355d63e319d67b8e206d1536ae8f7dfe3db
Yoshihiro TAKAHARA <y.takahara@gmail.com>
2010-02-27 17:21:12 +0900

    add

--- symfony-base.sh
+++ symfony-base.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 #symfony base
-yum install httpd -y
+rpm --import http://dag.wieers.com/rpm/packages/RPM-GPG-KEY.dag.txt
+rpm -ivh http://apt.sw.be/redhat/el5/en/i386/RPMS.dag/rpmforge-release-0.5.1-1.el5.rf.i386.rpm
+
+yum -y install httpd httpd-devel php php-pear php-devel git
+
+pear channel-discover pear.symfony-project.com
+pear install symfony/symfony-1.4.3
+
+mkdir -p /var/symfony-projects
+
 
