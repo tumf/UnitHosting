fb1669879e36e233fba4a9f38f18a05a4bc83f69
Yoshihiro TAKAHARA <y.takahara@gmail.com>
2010-02-28 22:20:09 +0900

    add

--- symfony-base.sh
+++ symfony-base.sh
@@ -8,7 +8,20 @@ rpm -ivh http://nog.dino.co.jp/dist/centos/5/dino/noarch/dino-release-1.0-1.noar
 rpm --import http://dag.wieers.com/rpm/packages/RPM-GPG-KEY.dag.txt
 rpm -ivh http://apt.sw.be/redhat/el5/en/i386/RPMS.dag/rpmforge-release-0.5.1-1.el5.rf.i386.rpm
 
-yum -y install httpd httpd-devel php php-pear php-devel php-dom git
+# yum install httpd & php
+yum -y install httpd httpd-devel php php-pear php-devel php-dom
+# yum install git
+yum -y git
+# yum install mysql
+yum -y install mysql mysql-server php-mysql
+sed -i.orig -e "s/\[mysqld\]/\[mysqld\]\ndefault-character-set = utf8/" /etc/my.cnf
+cat >> /etc/my.cnf <<EOF
+[client]
+default-character-set = utf8
+
+[mysqldump]
+default-character-set = utf8
+EOF
 
 # pear/pecl command patch
 sed -e 's/16M/640M/' < /usr/bin/pear  > /usr/bin/pear-extend_memory_limit
@@ -28,6 +41,14 @@ pear-extend_memory_limit install symfony/symfony
 # yum -y install expect
 
 # deploy先のディレクトリを作成
-mkdir -p /var/www/symfony/projects
+sudo -u uhuser mkdir -p /var/www/symfony/projects
+# sudo -u uhuser mkdir /home/uhuser/projects
+
+# virtual host の設定
+cat > /etc/httpd/conf.d/virtualhosts.conf <<EOF
+NameVirtualHost *:80
+Include site.d/*.conf
+EOF
+
 
 
