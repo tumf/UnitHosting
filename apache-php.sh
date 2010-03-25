97ec0b6c297cae237823c92090e71004bfdac0a8
Yoshihiro TAKAHARA <y.takahara@gmail.com>
2010-03-25 21:21:25 +0900

    add php-gd

--- apache-php.sh
+++ apache-php.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # yum install httpd & php
-yum -y install httpd httpd-devel php php-pear php-devel php-dom php-mbstring php-mysql
+yum -y install httpd httpd-devel php php-pear php-devel php-dom php-mbstring php-mysql php-gd
 # install APC
 yes yes|pecl install apc
 echo "extension=apc.so" > /etc/php.d/apc.ini
