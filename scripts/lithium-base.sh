bd032f33a9c886d43563f8011c69fc1b7188f8c1
Yusuke Ando <kabayaki91@Yusuke-Ando-no-MacBook-Pro.local>
2010-10-02 16:33:35 +0900

    added initial script for php5.3 and mongo

---
+++ scripts/lithium-base.sh
@@ -0,0 +1,16 @@
+# php5.3
+wget http://download.fedora.redhat.com/pub/epel/5/i386/epel-release-5-4.noarch.rpm
+wget http://rpms.famillecollet.com/enterprise/remi-release-5.rpm
+rpm -Uvh remi-release-5.rpm epel-release-5-4.noarch.rpm
+yum --enablerepo=remi install php
+/etc/init.d/httpd start
+
+# mongo db
+cd /usr/local/src
+wget http://downloads.mongodb.org/linux/mongodb-linux-i686-1.2.2.tgz
+tar zxfv mongodb-linux-i686-1.2.2.tgz
+
+# start up mongo
+mkdir /data
+mkdir /data/db
+/usr/local/src/mongodb-linux-i686-1.2.2/bin/mongod
