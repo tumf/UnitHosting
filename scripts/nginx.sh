ddd44615a0c954acf6712c307622fa46d1c00613
Yoshihiro TAKAHARA <y.takahara@gmail.com>
2012-02-21 18:03:22 +0900

    Update scripts/nginx.sh

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -1,13 +1,21 @@
-
 yum install -y pcre pcre-devel openssl openssl-devel gcc
 cd /usr/src
 wget http://nginx.org/download/nginx-0.9.4.tar.gz
 tar xvzf nginx-0.9.4.tar.gz
 cd  nginx-0.9.4
+sudo /usr/sbin/adduser nginx -d /var/lib/nginx -s /sbin/nologin 
+
 ./configure \
+    --user=nginx \
+    --group=nginx \
     --prefix=/var/lib/nginx \
-    --pid-path=/var/lib/nginx/run \
-    --lock-path=/var/lib/nginx/run \
-    --with-http_ssl_module
+    --with-http_ssl_module \
+    --http-fastcgi-temp-path=/var/lib/nginx/tmp/fastcgi_temp \
+    --http-proxy-temp-path=/var/lib/nginx/tmp/proxy_temp \
+    --http-fastcgi-temp-path=/var/lib/nginx/tmp/fastcgi_temp \
+    --http-client-body-temp-path=/var/lib/nginx/tmp/client_body_temp \
+    --http-uwsgi-temp-path=/var/lib/nginx/tmp/uwsgi_temp \
+    --http-scgi-temp-path=/var/lib/nginx/tmp/scgi_temp 
+make
 make install
     
